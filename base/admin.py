b28d0235f66ac02b7d981b718f07537c96457e0d
LASOV <44479103+Weestle@users.noreply.github.com>
2022-04-10 21:23:20 +0300

    1

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import Task
+from mptt.admin import MPTTModelAdmin
 
 admin.site.register(Task)
--- base/models.py
+++ base/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.urls import reverse
+from mptt.models import MPTTModel, TreeForeignKey
 from django.contrib.auth.models import User
+
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -8,9 +11,29 @@ class Task(models.Model):
     complete = models.BooleanField(default=False)
     create = models.DateTimeField(auto_now_add=True)
 
-
-    def  __str__(self):
+    def __str__(self):
         return self.title
 
     class Meta:
         ordering = ['complete']
+
+#
+# class Category(MPTTModel):
+#     title = models.CharField(max_length=50, unique=True, verbose_name='Название')
+#     parent = TreeForeignKey('self', on_delete=models.PROTECT, null=True, blank=True, related_name='children',
+#                             db_index=True, verbose_name='Родительская категория')
+#     slug = models.SlugField()
+#
+#     class MPTTMeta:
+#         order_insertion_by = ['title']
+#
+#     class Meta:
+#         unique_together = [['parent', 'slug']]
+#         verbose_name = 'Категория'
+#         verbose_name_plural = 'Категории'
+#
+#     def get_absolute_url(self):
+#         return reverse('post-by-category', args=[str(self.slug)])
+#
+#     def __str__(self):
+#         return self.title
